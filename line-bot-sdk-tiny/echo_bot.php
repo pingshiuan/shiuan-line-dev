f02042ccdf45053a8158446aad3d536349f269a0
pingshiuan <pingshiuan1995@gmail.com>
2017-01-14 00:45:26 +0800

    1

---
+++ line-bot-sdk-tiny/echo_bot.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = '6f52rgETE2k/FegR49SbG5ZKqB47nL9krkh90EBqLrq+vir1HvjRBJ/FLep0Al8UE4XTeRw94zglCv3nq/NEV0nIRAYNmg2djSAY26IcXFlatrepE9+VlMdQcuS/ZMZ84jGBgA0ndYRg+lED60i1MgdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '2b5de5d5b7097347bdfb7867154b77f4';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
